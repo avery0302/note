d1218f2e67a41aa5f057f31271fa8a7d086977f1
yuki <1335850330@qq.com>
2024-08-19 22:32:09 +0800

    删除:云服务器配置git步骤

--- 云服务器配置git远程pull步骤.sh
+++ 云服务器配置git远程pull步骤.sh
@@ -5,11 +5,11 @@ yum install git
 
 #生成ssh公钥私钥并分别配置(每次新建仓库都要重新生成)
 ssh-keygen -t rsa -b 4096 -C "133585xxxx@qq.com"
-在/root/.ssh生成了公钥文件和私钥文件
+在~/.ssh生成了公钥文件和私钥文件
 进入id_rsa.pub文件，复制公钥到github设置的ssh key列表中
 
 #克隆远程仓库
-cd 0-github
+cd github
 git clone git@github.com:avery0302/note.git
 
 #建立本地和远程分支映射
